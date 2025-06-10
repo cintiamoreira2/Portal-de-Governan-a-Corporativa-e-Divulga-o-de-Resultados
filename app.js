2662f1522e553e6d07194bfa3bcfb142e7ed35fa
Cintia Moreira <Cintia1350@gmail.com>
2025-06-10 20:44:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 angular.module('GovReportApp', [])
-.controller('LoginController', ['$scope', function($scope) {
-  $scope.user = {
-    email: '',
-    password: ''
+.directive('fileModel', ['$parse', function ($parse) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var model = $parse(attrs.fileModel);
+      element.bind('change', function() {
+        scope.$apply(() => model(scope).arquivo = element[0].files[0]);
+      });
+    }
   };
+}])
+.controller('LoginController', ['$scope', function($scope) {
+  $scope.user = { email: '', password: '' };
 
   $scope.login = function() {
     if ($scope.user.email && $scope.user.password) {
       alert('Login simulado para: ' + $scope.user.email);
-      // Aqui você chamaria sua API de autenticação real.
+      // Redirecionar para tela de envio (em app real)
+      window.location.href = "upload.html";
+    }
+  };
+}])
+.controller('UploadController', ['$scope', function($scope) {
+  $scope.doc = {};
+  $scope.documentos = [];
+
+  $scope.uploadDocument = function() {
+    if ($scope.doc.tipo && $scope.doc.titulo && $scope.doc.descricao && $scope.doc.arquivo) {
+      const novoDoc = {
+        tipo: $scope.doc.tipo,
+        titulo: $scope.doc.titulo,
+        data: new Date()
+      };
+      $scope.documentos.push(novoDoc);
+      alert("Documento enviado com sucesso!");
+
+      // Limpa o formulário
+      $scope.doc = {};
+      document.querySelector('input[type="file"]').value = null;
     }
   };
 }]);
